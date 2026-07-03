64d786e56ba52f90c8b8a62dcd41cd47908e3754
sajibsaha4647 <sksaha4648@gmail.com>
2026-07-03 14:39:47 +0600

    updated

--- app/src/main/java/com/example/two_days_challenge/Repo/HomeRepository.kt
+++ app/src/main/java/com/example/two_days_challenge/Repo/HomeRepository.kt
@@ -1,4 +1,8 @@
 package com.example.two_days_challenge.Repo
 
-class HomeRepository {
+import com.example.kotlin_project_structures_views.Network.ApiService
+
+class PostRepository(private val apiService: ApiService) {
+
+    suspend fun getPosts() = apiService.getPosts() ;
 }
